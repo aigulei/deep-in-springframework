12cb3a6043862b03e7b713c90310d3ca4e0651ef
aigulei <aigulei@github.com>
2020-06-10 23:15:57 +0800

    SpringBean的销毁

--- thinking-in-spring/spring-bean/src/main/java/com/ai/thinking/in/spring/bean/definition/BeanInitializationDemo.java
+++ thinking-in-spring/spring-bean/src/main/java/com/ai/thinking/in/spring/bean/definition/BeanInitializationDemo.java
@@ -19,11 +19,14 @@ public class BeanInitializationDemo {
         //非延迟初始化在Spring应用上下文初始化后执行
         System.out.println("Spring应用上下文已经启动...");
         UserFactory userFactory = applicationContext.getBean(UserFactory.class);
+        System.out.println(userFactory);
         //关闭
+        System.out.println("Spring应用上下文准备关闭...");
         applicationContext.close();
+        System.out.println("Spring应用上下文已经关闭...");
     }
 
-    @Bean(initMethod = "initUserFactory")
+    @Bean(initMethod = "initUserFactory",destroyMethod = "destroyFactory")
     @Lazy(value = false)
     public UserFactory userFactory(){
         return new DefaultUserFactory();
--- thinking-in-spring/spring-bean/src/main/java/com/ai/thinking/in/spring/bean/factory/DefaultUserFactory.java
+++ thinking-in-spring/spring-bean/src/main/java/com/ai/thinking/in/spring/bean/factory/DefaultUserFactory.java
@@ -1,13 +1,15 @@
 package com.ai.thinking.in.spring.bean.factory;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * {@link UserFactory}默认实现
  */
-public class DefaultUserFactory implements UserFactory, InitializingBean {
+public class DefaultUserFactory implements UserFactory, InitializingBean, DisposableBean {
     //1基于@PostConstruct注解
     @PostConstruct
     public void init(){
@@ -21,5 +23,23 @@ public class DefaultUserFactory implements UserFactory, InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("InitializingBean#afterPropertiesSet():UserFactory初始化中....");
+    }
+    @PreDestroy
+    public void preDestroy(){
+        System.out.println("@PreDestroy:UserFactory销毁中....");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("DisposableBean#destroy():UserFactory销毁中....");
+    }
+
+    public void destroyFactory(){
+        System.out.println("自定义销毁UserFactory");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("当前DefaultUserFactory正在被回收。。。");
     }
 }
