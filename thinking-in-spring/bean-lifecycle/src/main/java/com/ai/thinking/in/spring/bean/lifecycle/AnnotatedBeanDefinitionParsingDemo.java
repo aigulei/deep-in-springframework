1733536c5c393c024694de6fcf266d749ec94601
aigulei <aigulei@github.com>
2021-02-19 16:27:33 +0800

    面向注解BeanDefinition解析

---
+++ thinking-in-spring/bean-lifecycle/src/main/java/com/ai/thinking/in/spring/bean/lifecycle/AnnotatedBeanDefinitionParsingDemo.java
@@ -0,0 +1,20 @@
+package com.ai.thinking.in.spring.bean.lifecycle;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
+
+public class AnnotatedBeanDefinitionParsingDemo {
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        AnnotatedBeanDefinitionReader beanDefinitionReader = new AnnotatedBeanDefinitionReader(beanFactory);
+        int beanDefinitionCountBefore = beanFactory.getBeanDefinitionCount();
+        beanDefinitionReader.register(AnnotatedBeanDefinitionParsingDemo.class);
+        int beanDefinitionCountAfter = beanFactory.getBeanDefinitionCount();
+        int beanDefinitionCount = beanDefinitionCountAfter - beanDefinitionCountBefore;
+
+        System.out.println("已加载BeanDefinition数量:"+beanDefinitionCount);
+
+        AnnotatedBeanDefinitionParsingDemo demo = beanFactory.getBean("annotatedBeanDefinitionParsingDemo", AnnotatedBeanDefinitionParsingDemo.class);
+        System.out.println(demo);
+    }
+}
