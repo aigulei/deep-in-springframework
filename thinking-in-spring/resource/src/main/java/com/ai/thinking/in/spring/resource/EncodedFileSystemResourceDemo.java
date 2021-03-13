d0b00f558cb44d00d809eef4d661245019cfa035
aigulei <aigulei@github.com>
2021-03-13 12:48:14 +0800

    SpringResource接口扩展

---
+++ thinking-in-spring/resource/src/main/java/com/ai/thinking/in/spring/resource/EncodedFileSystemResourceDemo.java
@@ -0,0 +1,23 @@
+package com.ai.thinking.in.spring.resource;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.support.EncodedResource;
+
+import java.io.File;
+import java.io.Reader;
+
+/**
+ * {@link org.springframework.core.io.FileSystemResource}
+ */
+public class EncodedFileSystemResourceDemo {
+    public static void main(String[] args) throws  Exception{
+        String currentJavaFilePath = "D:\\coder\\workSpaceForSpring\\deep-in-springframework\\thinking-in-spring\\resource\\src\\main\\java\\com\\ai\\thinking\\in\\spring\\resource\\EncodedFileSystemResourceDemo.java";
+        File currentJavaFile = new File(currentJavaFilePath);
+        FileSystemResource fileSystemResource = new FileSystemResource(currentJavaFilePath);
+        EncodedResource encodedResource = new EncodedResource(fileSystemResource,"UTF-8");
+        //字符输入流
+        Reader reader = encodedResource.getReader();
+        System.out.println(IOUtils.toString(reader));
+    }
+}
