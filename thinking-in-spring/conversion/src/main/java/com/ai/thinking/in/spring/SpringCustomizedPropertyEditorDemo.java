a4a924f68a8e40a905bccb1838de0ab2b8cb463b
aigulei <aigulei@github.com>
2021-04-04 11:18:15 +0800

    Spring中的数据类型转换

--- thinking-in-spring/conversion/src/main/java/com/ai/thinking/in/spring/SpringCustomizedPropertyEditorDemo.java
+++ thinking-in-spring/conversion/src/main/java/com/ai/thinking/in/spring/SpringCustomizedPropertyEditorDemo.java
@@ -9,6 +9,13 @@ public class SpringCustomizedPropertyEditorDemo {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext =
                 new ClassPathXmlApplicationContext("classpath:/META-INF/property-editors-context.xml");
+        //AbstractApplicationContext -> "conversionService" ConversionService Bean
+        //-> ConfigurableBeanFactory#setConversionService -> AbstractBeanFactory#getConversionService ->BeanDefinition->BeanWrapper
+
+        //BeanDefinition -> BeanWrapper ->属性转换(数据来源：PropertyValue)
+        //setPropertyValue(PropertyValues) -> TypeConverter#convertIfNecessnary
+        //TypeConverterDelegate#convertInNecessnary -> PropertyEditor or ConversionService
+
         User user = applicationContext.getBean(User.class);
         System.out.println(user);
 
